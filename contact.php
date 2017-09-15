1bceb0218ab8fb37203a4fa84e534f602bf399f7
ThabangR <thabang.ramalatsoa@gmail.com>
2017-09-15 12:43:02 +0200

    Assign the correct email recipient.

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ $field_name = $_POST['name'];
 $field_email = $_POST['email'];
 $field_message = $_POST['message'];
 
-$mail_to = 'thabang.ramalatsoa@gmail.com';
+$mail_to = 'info@pkmconsult.co.za';
 $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
